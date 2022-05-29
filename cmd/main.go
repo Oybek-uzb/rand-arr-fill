067e80c2544f3aef3b2c9b7de56853c22f35ee82
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-05-30 01:13:31 +0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,10 @@
 package cmd
 
+import (
+	"log"
+	"rand-arr-fill/internal"
+)
+
 const (
 	rowsCount    = 5
 	columnsCount = 5
@@ -7,5 +12,26 @@ const (
 )
 
 func main() {
+	randomizer := internal.NewRandomizer()
+	randomIntegers, err := randomizer.GetUniqueRandomIntegers(internal.GetUniqueRandomIntegersInput{
+		SizeX:       rowsCount,
+		SizeY:       columnsCount,
+		RandomLimit: randomLimit,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	array, err := internal.NewIntArray(rowsCount, columnsCount)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := array.Fill(randomIntegers); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := internal.Print(array); err != nil {
+		log.Fatal(err)
+	}
 }
